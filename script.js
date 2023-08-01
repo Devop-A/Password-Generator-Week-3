22a171fabeec7b8803a55223eaf9ffd28975bad7
Austin Wilson <hunter.austin407@gmail.com>
2023-07-31 20:21:40 -0400

    Added prompt and paramters to function

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var length = prompt("Length 8-128 characters");
+  var lowerCase = confirm('Lowercase?');
+  var upperCase = confirm("Uppercase?");
+  var numbers = confirm("Numbers?");
+  var specialChars = confirm("Include special characters?");
+
+
+
+
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
